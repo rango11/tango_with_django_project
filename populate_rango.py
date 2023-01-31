5de3d6927372872abb58757172f05b0a5a87544b
Samir Mahmood <2659251M@student.gla.ac.uk>
2023-01-31 14:51:41 +0000

    Chapter 5, before exercises

--- populate_rango.py
+++ populate_rango.py
@@ -1,5 +1,5 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_projects.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
 
 
 import django
@@ -13,7 +13,7 @@ def populate():
 # This might seem a little bit confusing, but it allows us to iterate
 # through each data structure, and add the data to our models.
 
-python_pages = [
+    python_pages = [
     {'title': 'Official Python Tutorial',
     'url':'http://docs.python.org/3/tutorial/'},
     {'title':'How to Think like a Computer Scientist',
@@ -21,7 +21,7 @@ python_pages = [
     {'title':'Learn Python in 10 Minutes',
     'url':'http://www.korokithakis.net/tutorials/python/'} ]
 
-django_pages = [
+    django_pages = [
     {'title':'Official Django Tutorial',
     'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
     {'title':'Django Rocks',
@@ -29,19 +29,52 @@ django_pages = [
     {'title':'How to Tango with Django',
     'url':'http://www.tangowithdjango.com/'} ]
 
-other_pages = [
+    other_pages = [
     {'title':'Bottle',
     'url':'http://bottlepy.org/docs/dev/'},
     {'title':'Flask',
     'url':'http://flask.pocoo.org'} ]
 
-cats = {'Python': {'pages': python_pages},
+    java_pages = [
+        {'title': 'Java ArrayList', 'url': 'https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html'},
+        {'title': 'Java Design Patterns', 'url': 'https://www.javatpoint.com/design-patterns-in-java'}
+    ]
+
+    cats = {'Python': {'pages': python_pages},
         'Django': {'pages': django_pages},
-        'Other Frameworks':{'pages': other_pages} }
+        'Other Frameworks':{'pages': other_pages},
+        'Java': {'pages': java_pages}}
 
-#If you want to add more categories or pages,
-#add them to the dictionaries above.
+    #If you want to add more categories or pages,
+    #add them to the dictionaries above.
 
-#The code below goes through the cats dictionary, then adds each category,
-#and then adds all the associated pages for that category.
-#FINISH THIS
+    #The code below goes through the cats dictionary, then adds each category,
+    #and then adds all the associated pages for that category.
+
+    for cat, cat_data in cats.items():
+        c = add_cat(cat)
+        for p in cat_data['pages']:
+            add_page(c, p['title'], p['url'])
+
+    #Print out the categories we have added
+    for c in Category.objects.all():
+        for p in Page.objects.filter(category=c):
+            print(f'- {c}: {p}')
+
+def add_page(cat, title, url, views=0):
+    p = Page.objects.get_or_create(category = cat, title = title)[0]
+    p.url = url
+    p.views = views
+    p.save()
+    return p
+
+def add_cat(name):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.save()
+    return c
+
+#Satrt execution here
+if __name__ == '__main__':
+    print('Starting Rango Population script...')
+    populate()
+
