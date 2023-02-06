152488c830914c4569ca6dad1627dd7ad9e919c2
Samir Mahmood <2659251M@student.gla.ac.uk>
2023-02-06 13:50:08 +0000

    Chapter 5, making the tests work

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
 # Register your models here.
 from rango.models import Category, Page
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
--- rango/models.py
+++ rango/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique= True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default =0)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -18,3 +20,4 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
