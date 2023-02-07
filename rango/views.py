61dbb39f9a7c33dd82ef464e709dbd2617e01e62
Samir Mahmood <2659251M@student.gla.ac.uk>
2023-02-07 10:16:46 +0000

    Chapter 6, working exercises

--- rango/views.py
+++ rango/views.py
@@ -9,10 +9,12 @@ def index(request):
     #Retrievve the top 5 only -- or all if less than 5.
     #Place the list in our context_dict dictionary (with our boldmessage!) that will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
-    
+    page_list = Page.objects.order_by('-views')[:5]
+
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
 
     #Return a rendered response to send to the client.
     #We make use of the shortcut function to make our lives easier.
@@ -52,6 +54,7 @@ def show_category(request, category_name_slug):
 
     #Go render the response and return it to the client.
     return render(request, 'rango/category.html', context = context_dict)
+
     
 
 
